67273ff79dfb6292804f258f360fe85f163668de
Cameron Vogt <cvogt729@gmail.com>
2023-09-09 01:09:07 -0500

    auto-update-2

--- update.lua
+++ update.lua
@@ -1,16 +1,19 @@
 local args = {...}
 local term = require("term")
 local sh = require("shell")
+local rep = "https://raw.githubusercontent.com/cvogt729/opencomputers-scripts/main/"
 local path = debug.getinfo(2,"S").short_src
 local dir = path:gsub("[^/]*.lua$","")
 if #args==0 then
-  local ret = sh.execute("wget -f https://raw.githubusercontent.com/cvogt729/opencomputers-scripts/main/update.lua "..path)
+  local ret = sh.execute("wget -f "..rep.."update.lua "..path)
   if ret then
-    term.write("SUCCESS: update.lua\n")
-    sh.execute(path)
-  else
-    term.write("FAILED: update.lua\n")
+    sh.execute(path.." -x")
   end
   os.exit()
 end
-term.write("Test\n")
+local files = {
+  "mine.lua"
+}
+for i=#files,1,-1 do
+  sh.execute("wget -f "..rep..files[i].." "..dir..files[i])
+end
